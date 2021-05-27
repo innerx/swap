c0fc1341b8d69ce3f58e9403c457198736349c1f
chaos <xiaosaisai@hn-champion-credit.com>
2021-05-27 13:55:22 +0800

    hello controller

---
+++ src/test/java/com/example/swap/SwapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
